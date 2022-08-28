786e401705b675917e7ec49f017b3c45f1b19b35
linux-yankee <thegreatcubecrafter@gmail.com>
2022-08-27 20:43:38 -0700

    added bad comment rendering will improve soon tm

--- bin/main/js/post.js
+++ bin/main/js/post.js
@@ -31,12 +31,57 @@ function loadPost(author, title, body, createdTime, downs, ups, score, url){
     document.getElementById("url").innerHTML = url;
 }
 
+function loadComment(author, body, id, parent_comment_id, created_utc, downs, ups, score){
+
+    const commentsElement = document.getElementById("comments");
+    const bottomElement = document.getElementById("bottom");
+    const authorElement = document.createElement("p");
+    const bodyElement = document.createElement("p");
+    const createdElement = document.createElement("p");
+    const upsElement = document.createElement("p");
+    const scoreElement = document.createElement("p");
+    const downsElement = document.createElement("p");  
+
+    authorElement.setAttribute("id", "author-" + id);
+    authorElement.appendChild(document.createTextNode(author));
+    commentsElement.after(authorElement);
+
+    bodyElement.setAttribute("id", "author-body-" + id);
+    bodyElement.appendChild(document.createTextNode(body));
+    authorElement.after(bodyElement);
+
+    createdElement.setAttribute("id", "author-created-" + id);
+    createdElement.appendChild(document.createTextNode(unixToReadable(created_utc)));
+    bodyElement.after(createdElement);
+
+    upsElement.setAttribute("id", "author-ups-" + id);
+    upsElement.appendChild(document.createTextNode(ups));
+    createdElement.after(upsElement);
+    
+    scoreElement.setAttribute("id", "author-score-" + id);
+    scoreElement.appendChild(document.createTextNode(score));
+    upsElement.after(scoreElement);
+
+    downsElement.setAttribute("id", "author-downs-" + id);
+    downsElement.appendChild(document.createTextNode(downs));
+    commentsElement.after(downsElement);
+
+  /*try {
+  console.log(body);
+  const parent = document.getElementById(parent_comment_id);
+  parent.append(authorElement);
+  }
+  catch(err) {
+    document.getElementById("downs").append(authorElement);
+  }*/
+}
+
 function unixToReadable(timestamp) {
-    let date = new Date(timestamp);
+    let date = new Date(timestamp*1000);
     return ("Posted: " +
-          date.getDate() +
-          "/" + (date.getMonth()+1) +
-          "/" + date.getFullYear() +
+                date.getFullYear() +
+          "/" + date.getMonth() +
+          "/" + date.getDay() +
           " " + date.getHours() +
           ":" + date.getMinutes() +
           ":" + date.getSeconds());
@@ -52,7 +97,12 @@ function onLoad(){
         loadPost(data.author, data.title, data.body, data.createdTime, data.downs, data.ups, data.score, data.url);
     });
 
-    let comments = getJson("/json/reddit/comments/" +  postId).then(function(data) {
+    let comments = getJson("/json/reddit/comments/" +  postId).then(function(commentsList) {
+      //console.log(commentsList);
+      for (const comment of commentsList) {
+        console.log(comment);
+        loadComment(comment.author, comment.body, comment.id, comment.parent_comment_id, comment.createdTime, comment.downs, comment.ups, comment.score);
+      }
 
     });
 }
